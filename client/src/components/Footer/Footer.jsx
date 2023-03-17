2121a48758f3b238117fa4fe8ed71db38d3189d1
Kenny <knnyshn@gmail.com>
2023-03-17 11:05:14 -0400

    added boilerplate

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+// import logo from '../assets/logo.svg'
+
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        {/* <img className="" alt="" src={logo}/> */}
+        <h4>City</h4>
+        <h4>© 2023 Dumpster.io</h4>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
