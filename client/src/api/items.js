aa39d87352275f38a7a6c69a7294f4410502214b
Joseph Valle <josephrvalle@gmail.com>
2023-03-21 14:57:01 -0400

    lots of front-end work

--- client/src/api/items.js
+++ client/src/api/items.js
@@ -4,3 +4,8 @@ export async function getItems() {
   const res = await api.get('/new/')
   return res.data;
 }
+
+export async function getItem(_id) {
+  const res = await api.get(`/new/${_id}`)
+  console.log(res);
+}
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,18 +1,38 @@
-import logo from "../../../src/assets/icon.png";
+// Import CSS.
 import "./Header.css";
+
+// Import assets.
+import logo from "../../../src/assets/icon.png";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+
+// Import hooks.
+import { useRef, useState } from "react";
+
+// Import components.
 import BasicMenu from "./ProfileMenu";
 import SimpleBottomNavigation from "./BottomNav";
 
 function Header({ auth }) {
+  const [search, setSearch] = useState("")
+
+  const searchInput = useRef()
+
+  function handleSearch(e) {
+    console.log(searchInput.current.value)
+  }
   return (
     <div className="navbar">
       <img src={logo} alt="logo" className="navbar-logo" />
 
-
       <div className="search-bar">
-        <div className="search-bar-text">Search through the dumpster!</div>
-        <div className="search-icon-div">
+        <input
+          type="text"
+          className="search-field"
+          placeholder="Go diving..."
+          onChange={(e) => setSearch(e.target.value)}
+          ref={searchInput}
+        />
+        <div className="search-icon-div" onClick={handleSearch}>
           <SearchRoundedIcon className="search-icon" />
         </div>
       </div>
