1a4c2e82b316657eedcd81ae9783c096ffdb220d
MiguelArias01 <Argenis803@gmail.com>
2023-03-21 11:34:14 -0400

    added profile function

--- client/src/components/Header/ProfileMenu.jsx
+++ client/src/components/Header/ProfileMenu.jsx
@@ -46,7 +46,7 @@ export default function BasicMenu({ auth }) {
           <MenuRoundedIcon /> {auth ? <img alt="avatar" src={avatar} className="avatar-img" /> : <AccountCircleRoundedIcon />}
         </div>
         <Menu id="basic-menu" anchorEl={anchorEl} open={open} onClose={handleClose} MenuListProps={{ "aria-labelledby": "basic-button" }} sx={{ ".MuiPaper-root": { minWidth: "200px", borderRadius: "1rem", boxShadow: "0 1px 2px rgb(0 0 0 / 8%), 0 4px 12px rgb(0 0 0 / 5%)" }, }}>
-          <Link to="/user/profile">
+          <Link to="/user/profile/:id">
           <MenuItem className="menu-items" onClick={handleClose}> Profile</MenuItem>
           </Link>
           <MenuItem className="menu-items" onClick={handleClose}> Create Post</MenuItem>
