d91d9a1f55da89893e373435abe7b0f73f58e437
Kenny <knnyshn@gmail.com>
2023-03-22 11:33:17 -0400

    crud updates

--- client/src/api/items.js
+++ client/src/api/items.js
@@ -1,11 +1,16 @@
 import api from './apiConfig.js'
 
 export async function getItems() {
-  const res = await api.get('/new/')
+  const res = await api.get('/product/')
   return res.data;
 }
 
 export async function getItem(_id) {
-  const res = await api.get(`/new/${_id}`)
+  const res = await api.get(`/product/${_id}`)
   return res.data
+}
+
+export async function createProduct(product) {
+  const res = await api.post(`/product/`, product) 
+  return res.data   
 }
