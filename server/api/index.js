f221311b27015ce97a3a5e994d89f767cdee9ab0
Joseph Valle <josephrvalle@gmail.com>
2023-03-23 11:00:53 -0400

    still trying to get vercel to link

--- server/api/index.js
+++ server/api/index.js
@@ -1,30 +1,13 @@
 import 'dotenv/config'
+import connection from '../db/connection.js'
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
-import lifecycle from './middleware/middleware.js'
-
 import authRouter from '../routes/auth.js'
 import productRouter from '../routes/product.js'
 import userRouter from '../routes/userRoute.js'
 
-const PORT = process.env.PORT || 8080
-
 const app = express()
-
-app.use(lifecycle({
-  async setup() {
-    // This runs before all your handlers
-    // Put your database connection here. e.g.
-    // mongoose.set('strictQuery', false)
-    // await mongoose.connect(process.env.DATABASE_URL)
-  },
-  async cleanup() {
-    // This runs after all your handlers
-    // Put your database disconnection here. e.g.
-    // await mongoose.disconnect()
-  }
-}))
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json())
@@ -34,9 +17,5 @@ app.use(morgan('dev'))
 app.use('/api/auth/', authRouter)
 app.use('/api/product/', productRouter)
 app.use('/api/user/', userRouter)
-
-// app.listen(PORT, () => {
-//   console.log(`Server started at http://localhost:${PORT}`);
-// })
 
 export default app
